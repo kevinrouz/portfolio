6eb8a742e095cbe132bf0075beaa30c6f3ad94cf
Kevin Farokhrouz <kevinafarokhrouz@gmail.com>
2024-11-26 03:28:14 -0600

    Fix image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -52,7 +52,7 @@ export default function Hero() {
             transition={{ duration: 0.8 }}
             >
             <Image
-                src="/k3.jpg"
+                src="/k3.JPG"
                 alt="Kevin Farokhrouz - Photo"
                 width={256}
                 height={256}
