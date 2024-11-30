646df339ab01c066be54c65107233061978d5b8d
Kevin Farokhrouz <kevinafarokhrouz@gmail.com>
2024-11-30 13:27:21 -0600

    GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -32,6 +33,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-42BRBQ40LQ" />
+        <Script  id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-42BRBQ40LQ');
+            `}
+        </Script>
+      </head>
       <body className={`${inter.className} bg-black text-white min-h-screen`}>
         {children}
       </body>
